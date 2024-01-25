fcade3944cb1143e87fa0ef2b211497570da20f7
dboicourt <dboicourt@gmail.com>
2024-01-24 16:09:59 -0800

    Fixed formatting

--- book-club/ClientApp/src/components/ClubSidebar.js
+++ book-club/ClientApp/src/components/ClubSidebar.js
@@ -1,16 +1,16 @@
-import React, {useContext, useState} from 'react';
+import React, { useContext, useState } from 'react';
 
 export function ClubSidebar({ clubsArray = [] }) {
-    return(
+    return (
         <div id="clubSidebar" className="bg-green-700 h-screen w-sidebarWidth">
-            <ul>{ 
+            <ul>{
                 clubsArray.map(function (club, i) {
                     return (
                         <li key={i}>{club.name}</li>
                     )
                 })
             }
-        </ul>
+            </ul>
         </div>
     )
 }
--- book-club/ClientApp/src/components/Layout.js
+++ book-club/ClientApp/src/components/Layout.js
@@ -4,29 +4,29 @@ import { NavMenu } from './NavMenu';
 import { ClubSidebar } from './ClubSidebar';
 
 export class Layout extends Component {
-  static displayName = Layout.name;
+    static displayName = Layout.name;
 
 
-  render() {
+    render() {
 
-      //todo:pull in data for clubs attatched to user and pass along to the ClubSidebar component
-      const testClubArr = [{ name: "test" }, { name: "testtwo" }]
+        //todo:pull in data for clubs attatched to user and pass along to the ClubSidebar component
+        const testClubArr = [{ name: "test" }, { name: "testtwo" }]
 
-    const wrapper = {
-      display: "flex"
+        const wrapper = {
+            display: "flex"
+        }
+
+        return (
+            <div>
+                <NavMenu />
+                <div style={wrapper}>
+                    <ClubSidebar clubsArray={testClubArr} style={{ flex: "1 1 auto" }} />
+                    <Container style={{ flex: "1 8 auto" }}>
+                        {this.props.children}
+                    </Container>
+                    <ClubSidebar style={{ flex: "1 1 auto" }} />
+                </div>
+            </div>
+        );
     }
-
-    return (
-      <div>
-        <NavMenu />
-        <div style={wrapper}>
-        <ClubSidebar clubsArray={testClubArr} style={{flex:"1 1 auto"}}/>
-        <Container style={{ flex: "1 8 auto" }}>
-          {this.props.children}
-        </Container>
-        <ClubSidebar style={{ flex: "1 1 auto" }} />
-        </div>
-      </div>
-    );
-  }
 }
