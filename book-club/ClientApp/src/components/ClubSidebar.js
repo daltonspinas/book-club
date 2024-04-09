2be04100e4bf6f6d011fd115501776fa28e23ea1
dboicourt <dboicourt@gmail.com>
2024-04-09 16:18:44 -0700

    Wired up API to populate Title and Author fields. Moved directly to component

--- book-club/ClientApp/src/components/ClubSidebar.js
+++ book-club/ClientApp/src/components/ClubSidebar.js
@@ -1,5 +1,4 @@
 import React, { useContext, useState } from 'react';
-import { getBookByID } from '../apis/BooksApi';
 
 export function ClubSidebar({ clubsArray = [] }) {
     return (
--- book-club/ClientApp/src/components/Home.js
+++ book-club/ClientApp/src/components/Home.js
@@ -1,11 +1,32 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import { AppContext } from '../context/GlobalState';
+import axios from "axios";
 
 export function Home() {
-  const {bookImage, bookTitle, bookAuthor, meetingDate, meetingHost, meetingAddress} = useContext(AppContext)  
-  return(
-    <div className='bg-indigo-500 w-mainWidth h-screen grid items-center justify-center max-h-screen gap-2'>
-      <img className='justify-self-center' src={bookImage}></img>
+    const { bookImage, bookTitle, setBookTitle, bookAuthor, setBookAuthor, meetingDate, meetingHost, meetingAddress } = useContext(AppContext)
+
+    //Books API
+    const client = axios.create({
+        baseURL: "https://www.googleapis.com/books/v1/volumes/"
+    })
+
+    const practiceID = "OXzLsgEACAAJ?key=AIzaSyB8Qt63pYxZHRNxlDJQQUYQLS4Zuqcw5ZU"
+
+    React.useEffect(() => {
+        client.get(practiceID).then((response) => {
+            let resultObj = {
+                title: response.data.volumeInfo.title,
+                //authors comes back as an array, may want to modify this later to handle multiple authors. For now assume first result is main/only author
+                author: response.data.volumeInfo.authors[0],
+            }
+            setBookTitle(resultObj.title);
+            setBookAuthor(resultObj.author);
+        })
+    }, []);
+
+    return (
+        <div className='bg-indigo-500 w-mainWidth h-screen grid items-center justify-center max-h-screen gap-2'>
+      <img className='justify-self-center' src={bookImage} alt="bookImage"></img>
       <h1 className='justify-self-center text-4xl'>{bookTitle}</h1>
       <h2 className='justify-self-center text-3xl'>{bookAuthor}</h2>
       <h3 className='justify-self-center text-xl'>Next Meeting: {meetingDate}</h3>
