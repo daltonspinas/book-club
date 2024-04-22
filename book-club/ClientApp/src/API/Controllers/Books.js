20550f318d973ed64a0df04529e0564dca2540e3
dboicourt <dboicourt@gmail.com>
2024-04-22 10:49:11 -0700

    Initial Add of BooksAPI

---
+++ book-club/ClientApp/src/API/Controllers/Books.js
@@ -0,0 +1,14 @@
+import {api} from '../googleAxiosConfig'
+
+const controllerBase = '/v1/volumes'
+
+export const booksAPI = {
+
+    getBook: async function(bookID) {
+        const response = await api.request({
+            url: `${controllerBase}/${bookID}`,
+            method: 'GET',
+        })
+        return response.data
+    }
+}
---
+++ book-club/ClientApp/src/API/googleAxiosConfig.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { useEffect } from "react";
+
+export const api = axios.create({
+  baseURL: "https://www.googleapis.com/books",
+});
+
+// defining a custom error handler for all APIs
+const errorHandler = (error) => {
+  const statusCode = error.response?.status;
+  console.error(statusCode, error);
+
+  return Promise.reject(error);
+};
+
+export default function AxiosConfig() {
+
+  // wrap in useEffect so we don't register a new interceptor every time
+  useEffect(() => {
+    api.interceptors.response.use(undefined, (error) => {
+      return errorHandler(error);
+    });
+
+    api.interceptors.request.use((config) => {
+		// attach the jwt to outbound api calls
+      //const accessToken = localStorage.getItem('accessToken')
+      //config.headers.Authorization = "Bearer " + accessToken;
+      return config;
+    });
+  }, []);
+}
